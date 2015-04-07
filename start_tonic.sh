a83783d276d3f45761b439a9e179b8799ec68dde
Alexandru Tache <hadesgames@gmail.com>
2015-04-07 05:11:41 +0300

    Tonic

--- start_tonic.sh
+++ start_tonic.sh
@@ -1,1 +1,1 @@
-exec docker run --rm -i -t -e UPSTREAM_URL=http://nginx:80 --link nginx:nginx -p 4000:4000 --name=tonic tonic
+exec docker run --rm -i -t -e UPSTREAM_URL=http://nginx:80 -e REDIS_HOST="redis" --link redis:redis --link nginx:nginx -p 4000:4000 --name=tonic tonic
